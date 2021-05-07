d70a9765dffaf2624fbfb345431bdbcbbec1b4ae
Monica-Hall <monicakes@Monicas-MacBook-Air.local>
2021-05-07 16:15:47 -0700

    completed stories up to 13, still work in progress

--- index.js
+++ index.js
@@ -18,22 +18,72 @@ let drawCanvas = () => {
 
 let generateScales = () => {
     xScale = d3.scaleLinear()
+        //- User Story #11: I can see that the range of the x-axis labels are within the range of the actual x-axis data.
+        .domain([d3.min(values, (item) => {
+            return item["Year"]
+        }) -1, d3.max(values, (item) => {
+            return item["Year"]
+        }) + 1])
         .range([padding, width - padding])
 
     yScale = d3.scaleTime()
+        // - User Story #12: I can see that the range of the y-axis labels are within the range of the actual y-axis data.
+        .domain([d3.min(values, (item) => {
+            return new Date(item["Seconds"] * 1000)
+        }), d3.max(values, (item) => {
+            return new Date(item["Seconds"] * 1000)
+        })])
         .range([padding, height - padding])
 }
 
 let drawPoints = () => {
+    svg.selectAll("circle")
+        .data(values)
+        .enter()
+        .append("circle")
+        // -User Story #4: I can see dots, that each have a class of dot, which represent the data being plotted.
+        .attr("class", "dot")
+        .attr("r", "5")
+        // -User Story #5: Each dot should have the properties data-xvalue and data-yvalue containing their corresponding x and y values.
+            // -User Story #6: The data-xvalue and data-yvalue of each dot should be within the range of the actual data and in the correct data format. For data-xvalue, integers (full years) or Date objects are acceptable for test evaluation. For data-yvalue (minutes), use Date objects.
+                // - User Story #7: The data-xvalue and its corresponding dot should align with the corresponding point/value on the x-axis.
+        .attr("data-xvalue", (item) => {
+            return item["Year"]
+        })
+                // -User Story #8: The data-yvalue and its corresponding dot should align with the corresponding point/value on the y-axis.
+        .attr("data-yvalue", (item) => {
+            return new Date(item["Seconds"] * 1000)
+        })
 
+        .attr("cx",  (item) => {
+            return xScale(item["Year"])
+        })
+
+        .attr("cy", (item) => {
+            return yScale(new Date(item["Seconds"] * 1000))
+        })
+
+        .attr("fill", (item) => {
+            if(item["Doping"] != "") {
+                return "red"
+            } else {
+                return "white"
+            }
+        })
 }
 
 let generateAxis = () => {
     // generate x axis 
     let xAxis = d3.axisBottom(xScale)
+                // - User Story #10: I can see multiple tick labels on the x-axis that show the year.
+                // to put year in interger form 
+                .tickFormat(d3.format("d"))
 
     // generate y axis 
     let yAxis = d3.axisLeft(yScale)
+                // - User Story #9: I can see multiple tick labels on the y-axis with %M:%S time format.
+                // to put in proper time format in minutes and seconds 
+                .tickFormat(d3.timeFormat("%M:%S"))
 
     svg.append("g")
     .call(xAxis)
